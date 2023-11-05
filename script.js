39db60088a50183fc5c91bf893c5288dd80ea8f7
Syed Kumail Raza <136350543+syedkumailraza1@users.noreply.github.com>
2023-11-05 17:34:27 +0530

    Completed

---
+++ script.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    alert("Humpe To Haiye Na");
+}
+
+function openInstagram() {
+    
+    var instagramURL = "https://www.instagram.com/its_kumailrazas/";
+    window.open(instagramURL, '_blank');
+}
