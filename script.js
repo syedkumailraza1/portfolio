82a61af02960da22254b24d8c183c82655c7fd42
Syed Kumail Raza <136350543+syedkumailraza1@users.noreply.github.com>
2024-02-05 22:56:07 +0530

    portfolio updated

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function openInstagram() {
     var instagramURL = "https://www.instagram.com/its_kumailrazas/";
     window.open(instagramURL, '_blank');
 }
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("name").style.padding = "30px 10px";
+  } 
+}
